5ad4d905952ce743e86481633ce783a041122f49
Luka Gogichaishvili <luka595664@gmail.com>
2026-02-05 13:30:38 +0400

    feat: calculator elements

--- mortgage/app/page.tsx
+++ mortgage/app/page.tsx
@@ -1,3 +1,28 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <section id="calculator-form">
+        <h1>Mortgage Calculator</h1>
+        <p>Clear All</p>
+        <div>
+          <label htmlFor="amount">Mortgage Amount</label>
+          <input type="number" name="amount" />
+          <label htmlFor="years">Mortgage Term</label>
+          <input type="number" name="years" />
+          <label htmlFor="rate">Interest Rate</label>
+          <input type="number" name="rate" />
+          <label htmlFor="type">Mortgage Type</label>
+          <label>
+            <input type="radio" name="type" value="repayment" /> Repayment
+          </label>
+          <label>
+            <input type="radio" name="type" value="interest-only" /> Interest
+            Only
+          </label>
+        </div>
+        <button>Calculate Repayments</button>
+      </section>
+      <section id="result"></section>
+    </main>
+  );
 }
