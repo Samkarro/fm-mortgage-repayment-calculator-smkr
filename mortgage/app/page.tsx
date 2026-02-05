b7d6c630b4d76ab04bc86f9f9fe517a372685b54
Luka Gogichaishvili <luka595664@gmail.com>
2026-02-05 17:07:32 +0400

    feat: results section layout

--- mortgage/app/page.tsx
+++ mortgage/app/page.tsx
@@ -1,6 +1,11 @@
+"use client";
+import { useState } from "react";
 import "./calculator.css";
+import "./result.css";
 
 export default function Home() {
+  const [result, setResult] = useState<number>(0);
+
   return (
     <main>
       <section id="calculator-form">
@@ -46,7 +51,36 @@ export default function Home() {
           Calculate Repayments
         </button>
       </section>
-      <section id="result"></section>
+      <section id="result">
+        {result == 0 ? (
+          <div className="results-container">
+            <img
+              src="./assets/images/illustration-empty.svg"
+              alt="Abstract drawing of a calculation sheet with money, a pen and calculator floating around it."
+            />
+            <h2>Results shown here</h2>
+            <p>
+              Complete the form and click "calculate repayments" to see what
+              your monthly repaiments would be.
+            </p>
+          </div>
+        ) : (
+          <div className="results-container success">
+            <h2>Results shown here</h2>
+            <p>
+              Complete the form and click "calculate repayments" to see what
+              your monthly repaiments would be.
+            </p>
+            <div className="result-card">
+              <p>Your monthly repayments</p>
+              <p className="repayment-amount">{result}</p>
+              <hr />
+              <p>Total you'll repay over the term</p>
+              <p className="total-amount"></p>
+            </div>
+          </div>
+        )}
+      </section>
     </main>
   );
 }
